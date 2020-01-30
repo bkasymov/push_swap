5ee85b6cad80ee4ffb73979ea01b861a9e15da51
kololokola <behaqosimi@gmail.com>
2020-01-30 23:35:12 +0300

    I think than checker is ready

--- src/checker.c
+++ src/checker.c
@@ -1,12 +1,92 @@
 #include "../includes/push_swap.h"
+#include "stdio.h"
+
+void    ft_perfroming_rules(char *line, t_vars *chv)
+{
+	if (!(ft_strcmp(line, "sa")))
+		ft_sa(chv, 0);
+	else if (!(ft_strcmp(line, "sb")))
+		ft_sb(chv, 0);
+	else if (!(ft_strcmp(line, "ss")))
+		ft_ss(chv, 0);
+	else if (!(ft_strcmp(line, "ra")))
+		ft_ra(chv, 0);
+	else if (!(ft_strcmp(line, "rb")))
+		ft_rb(chv, 0);
+	else if (!(ft_strcmp(line, "rr")))
+		ft_rr(chv, 0);
+	else if (!(ft_strcmp(line, "rra")))
+		ft_rra(chv, 0);
+	else if (!(ft_strcmp(line, "rrb")))
+		ft_rrb(chv, 0);
+	else if (!(ft_strcmp(line, "rrr")))
+		ft_rrr(chv, 0);
+	else if (!(ft_strcmp(line, "pa")))
+		ft_pa(chv, 0);
+	else if (!(ft_strcmp(line, "pb")))
+		ft_pb(chv, 0);
+	else
+		error_print(chv);
+}
+
+void    ft_read_rules(t_vars *chv)
+{
+	char    *line;
+
+	line = NULL;
+	while (get_next_line(0, &line))
+	{
+		ft_perfroming_rules(line, chv);
+		free(line);
+		line = NULL;
+	}
+	if (line)
+	{
+		free(line);
+		line = NULL;
+	}
+}
+
+
+int     ft_check_sort(t_vars *chv)
+{
+	t_stack     *tmp;
+	int         i;
+	int         digit;
+	int         j;
+
+	i = 0;
+	digit = 0;
+	j = 0;
+	tmp = chv->stack_a;
+	while (i < chv->qa)
+	{
+		if (digit < tmp->data)
+		{
+			digit = tmp->data;
+			j++;
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	if (j == i - 1)
+		return (1);
+	return (0);
+}
 
 int     main(int argc, char **argv)
 {
 	t_vars      chv;
-	char        **line;
 
-	line = (char **)malloc(sizeof(char) * 10000);
 	if ((ft_general_parser(argc, argv, &chv)) == 1)
 		return (1);
-	get_next_line(0, line);
+	if (ft_is_it_sorted(&chv))
+		return (0);
+	ft_check_dupl(&chv);
+	ft_read_rules(&chv);
+	if (ft_check_sort(&chv))
+		write(1, "OK\n", 3);
+	else
+		write(1, "KO\n", 3);
+	return (0);
 }
